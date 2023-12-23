bcf511af81406833f6ed9473b4dc4bd53dfcdbf8
Ahsan Shah <shahahsan56@gmail.com>
2023-12-23 21:32:44 +0500

    updated

--- dto/todo.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class TODOCreate(BaseModel):
-   text: str
-   completed: bool
-
-class TODOUpdate(TODOCreate):
-   id: int
-
--- dto/user.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, EmailStr
-
-class UserBase(BaseModel):
-   email: EmailStr
-
-class UserCreate(UserBase):
-   name: str
-   password: str
